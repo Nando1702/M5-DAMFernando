e3fcda06244c0369c981bca9007a352213c39b72
usuario <usuario@DESKTOP-2GA9RPP>
2023-02-26 12:47:15 +0100

    version 3, añadido metodos

--- Main.java
+++ Main.java
@@ -2,51 +2,43 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import pkg_utilitats.Metodos;
 
 class Main {
 
     public static void main(String[] args) {
-        int num = 40;
+        final int num = 40;
+
 
 //vector de notes generades
-        Integer[] notasExamen = new Integer[num];
-        int max = 0;
-        int min = 0;
+        Integer[] notasExamen;
+        Integer[] notasPractica;
+        int max;
+        int min;
         int indMaxNota, indMinNota;
-        int postEval;
 
-        //Genera notes random entre 1 i 10
-        for (int i = 0; i < notasExamen.length; i++) {
-            notasExamen[i] = (int) (Math.random() * 11);
-        }
-        //Es busca la mes gran
-        postEval = notasExamen[0];
-        for (int i = 1; i < notasExamen.length; i++) {
-            int preEval = notasExamen[i];
-            if (preEval < postEval) {
-                min = preEval;
-                postEval = notasExamen[i];
-            }
-        }
+
+// Genera notes random entre 1 i 10
+
+        notasExamen = Metodos.generarNotas(num);
+        notasPractica = Metodos.generarNotas(num);
+        
+        
+//Es busca la mes gran
+        
+        max = Metodos.buscarNota(notasExamen, "max");
+
         //Es busca la mes petita
-        postEval = notasExamen[0];
-        for (int i = 1; i < notasExamen.length; i++) {
-            int preEval = notasExamen[i];
-            if (preEval > postEval) {
-                max = preEval;
-                postEval = notasExamen[i];
-            }
-        }
+        
+        min = Metodos.buscarNota(notasExamen, "min");
+
 
 //Alumnes de la classe, con un contador basta 
 
-//        int[] listaClase = new int[num];
-//        for (int i = 1; i < num; i++) {
-//            listaClase[i] = i + 1;
-//        }
 
 //LLista per a facilitar us de index
-        List notes = Arrays.asList(notasExamen);
+        
+List notes = Arrays.asList(notasExamen);
         indMinNota = notes.indexOf(min) + 1;
         indMaxNota = notes.indexOf(max) + 1;
 
@@ -55,96 +47,37 @@ class Main {
         System.out.println("Màxim és: " + max);
         System.out.println("Index del mínim és : " + indMinNota);
         System.out.println("Index del màxim és : " + indMaxNota);
-//    System.out.println("Lista de classe :" + Arrays.toString(listaClase));
+
         System.out.println("Array de Notes :" + notes);
 
-        //generamos las notes  de las practicas "
-        int[] notaPracticas = new int[num];
-        for (int i = 0; i < notaPracticas.length; i++) {
-            notaPracticas[i] = (int) (Math.random() * 11);
-        }
-        //qualificacions
-        float[] notaFinal = new float[num];
-        for (int i = 0; i < notasExamen.length; i++) {
-            notaFinal[i]
-                    = (((float) notasExamen[i]
-                    + (float) notaPracticas[i])
-                    / 2);
-        }
+ 
+// generamos qualificacions
+
+        float[] notaFinal = Metodos.notaFinals(notasExamen, notasPractica);
         
-        System.out.println("Práctiques      :" + Arrays.toString(notaPracticas));
+        System.out.println("Práctiques      :" + Arrays.toString(notasPractica));
         System.out.println("Qualificacions :" + Arrays.toString(notaFinal));
 
         //estadística de qualif
-        float[] estadistica = new float[10];
-
-        for (int i = 0; i < 10; i++) {
-            float count = 0;
-            float sum = 0;
-            for (int j = 0; j < notasExamen.length; j++) {
-                if ((i < notaFinal[j]) && ((i + 1) >= notaFinal[j])) {
-                    sum += notaFinal[j];
-                    count += 1;
-                }
-            }
-            if (count != 0) {
-                estadistica[i] = ((float) count / num);
-            } else {
-                estadistica[i] = 0;
-            }
-            double sol = (Math.round(estadistica[i] * 10000.0)) / 100.0;
-            System.out.println("Estadística nota tram <="
-                    + (i + 1) + " = "
-                    + sol + "%");
-        }
+        
+        float[] estadistica = Metodos.estadisticaNotas(notaFinal);
+        Metodos.impEstadistica(estadistica);
+        
+           
         //Aprovats i suspesos
-        int[] aprovats = new int[num];
-        int[] supesos = new int[num];
-        int cAprovats = 0;
-        int cSuspesos = 0;
-        for (int i = 0; i < num; i++) {
-            if (notaFinal[i] < 5) {
-                aprovats[i] = i;
-                cAprovats += 1;
-            } else {
-                supesos[i] = i;
-                cSuspesos += 1;
-            }
-        }
+        ArrayList<Integer> aprovats = Metodos.auxNotas(notaFinal, 10, 5);
+        ArrayList<Integer> suspendidos = Metodos.auxNotas(notaFinal, 5, 0);
+
         System.out.println("Relació d'aprovats per nº de llista: "
-                + Arrays.toString(aprovats));
+                + aprovats);
         System.out.println("Relació d'aprovats per nº de lista: "
-                + Arrays.toString(supesos));
+                + suspendidos);
 
-        //Resumen de aprovats i supesos
-        int i = 0;
-        int x = 0;
-        int[] a = new int[cAprovats];
-        int[] s = new int[cSuspesos];
-        while (i < aprovats.length) {
-            if (aprovats[i] != 0) {
-                a[x] = aprovats[i];
-                i++;
-                x++;
-            } else {
-                i++;
-            }
-        }
 
-        i = x = 0;
-        while (i < supesos.length) {
-            if (supesos[i] != 0) {
-                s[x] = supesos[i];
-                i++;
-                x++;
-            } else {
-                i++;
-            }
-        }
-        System.out.println("Resum aprovats per nº de llista: "
-                + Arrays.toString(a));
-        System.out.println("Resum suspesos per nº de llista: "
-                + Arrays.toString(s));
+   
+       
+        
+       
 
     }
 
