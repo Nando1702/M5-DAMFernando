c50ada862a7e43ec03c2e6aba9ce7141b36a888d
usuario <usuario@DESKTOP-2GA9RPP>
2023-02-26 10:47:48 +0100

    version 2, he cambiado algunos nombres de variables

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,42 +8,45 @@ class Main {
     public static void main(String[] args) {
         int num = 40;
 
-        //vector de notes generades
-        Integer[] control = new Integer[num];
+//vector de notes generades
+        Integer[] notasExamen = new Integer[num];
         int max = 0;
         int min = 0;
         int indMaxNota, indMinNota;
         int postEval;
 
         //Genera notes random entre 1 i 10
-        for (int i = 0; i < control.length; i++) {
-            control[i] = (int) (Math.random() * 11);
+        for (int i = 0; i < notasExamen.length; i++) {
+            notasExamen[i] = (int) (Math.random() * 11);
         }
         //Es busca la mes gran
-        postEval = 11;
-        for (int i = 0; i < control.length; i++) {
-            int preEval = control[i];
+        postEval = notasExamen[0];
+        for (int i = 1; i < notasExamen.length; i++) {
+            int preEval = notasExamen[i];
             if (preEval < postEval) {
                 min = preEval;
-                postEval = control[i];
+                postEval = notasExamen[i];
             }
         }
         //Es busca la mes petita
-        postEval = 0;
-        for (int i = 0; i < control.length; i++) {
-            int preEval = control[i];
+        postEval = notasExamen[0];
+        for (int i = 1; i < notasExamen.length; i++) {
+            int preEval = notasExamen[i];
             if (preEval > postEval) {
                 max = preEval;
-                postEval = control[i];
+                postEval = notasExamen[i];
             }
         }
-        //Alumnes de la classe
-        int[] listaClase = new int[num];
-        for (int i = 0; i < num; i++) {
-            listaClase[i] = i + 1;
-        }
-        //LLista per a facilitar us de index
-        List notes = Arrays.asList(control);
+
+//Alumnes de la classe, con un contador basta 
+
+//        int[] listaClase = new int[num];
+//        for (int i = 1; i < num; i++) {
+//            listaClase[i] = i + 1;
+//        }
+
+//LLista per a facilitar us de index
+        List notes = Arrays.asList(notasExamen);
         indMinNota = notes.indexOf(min) + 1;
         indMaxNota = notes.indexOf(max) + 1;
 
@@ -51,24 +55,25 @@ class Main {
         System.out.println("Màxim és: " + max);
         System.out.println("Index del mínim és : " + indMinNota);
         System.out.println("Index del màxim és : " + indMaxNota);
-        System.out.println("Lista de classe :" + Arrays.toString(listaClase));
+//    System.out.println("Lista de classe :" + Arrays.toString(listaClase));
         System.out.println("Array de Notes :" + notes);
 
-        //notes "practiques"
-        int[] pract = new int[num];
-        for (int i = 0; i < pract.length; i++) {
-            pract[i] = (int) (Math.random() * 11);
+        //generamos las notes  de las practicas "
+        int[] notaPracticas = new int[num];
+        for (int i = 0; i < notaPracticas.length; i++) {
+            notaPracticas[i] = (int) (Math.random() * 11);
         }
         //qualificacions
-        float[] qualif = new float[num];
-        for (int i = 0; i < control.length; i++) {
-            qualif[i]
-                    = (((float) control[i]
-                    + (float) pract[i])
+        float[] notaFinal = new float[num];
+        for (int i = 0; i < notasExamen.length; i++) {
+            notaFinal[i]
+                    = (((float) notasExamen[i]
+                    + (float) notaPracticas[i])
                     / 2);
         }
-        System.out.println("Práctiques      :" + Arrays.toString(pract));
-        System.out.println("Qualificacions :" + Arrays.toString(qualif));
+        
+        System.out.println("Práctiques      :" + Arrays.toString(notaPracticas));
+        System.out.println("Qualificacions :" + Arrays.toString(notaFinal));
 
         //estadística de qualif
         float[] estadistica = new float[10];
@@ -76,9 +81,9 @@ class Main {
         for (int i = 0; i < 10; i++) {
             float count = 0;
             float sum = 0;
-            for (int j = 0; j < control.length; j++) {
-                if ((i < qualif[j]) && ((i + 1) >= qualif[j])) {
-                    sum += qualif[j];
+            for (int j = 0; j < notasExamen.length; j++) {
+                if ((i < notaFinal[j]) && ((i + 1) >= notaFinal[j])) {
+                    sum += notaFinal[j];
                     count += 1;
                 }
             }
@@ -98,7 +103,7 @@ class Main {
         int cAprovats = 0;
         int cSuspesos = 0;
         for (int i = 0; i < num; i++) {
-            if (qualif[i] < 5) {
+            if (notaFinal[i] < 5) {
                 aprovats[i] = i;
                 cAprovats += 1;
             } else {
